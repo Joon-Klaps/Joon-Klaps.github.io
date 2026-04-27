741b745e745703414810343923f8c5a74d3897f6
Joon-Klaps <joon.klaps@kuleuven.be>
2026-04-27 11:27:20 +0200

    add rapidtrees project

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,5 @@ export const SITE_TITLE = "Joon Klaps";
 export const SITE_DESCRIPTION = "Personal website for Joon Klaps, a bioinformatician living in Belgium 🇧🇪.";
 
 export const GOOGLE_SITE_VERIFICATION = "g_-UEYoHNh9r6fT0p49sMm9DN-tKX_Yg1nW7QuAD8mE";
+
+export const MAX_HOMEPAGE_ICONS = 4;
